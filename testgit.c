b2ff1f4f711f21628da69e7ed31d0452d1d8e170
folkdogfk <talesrunnerfolk@gmail.com>
2018-01-26 17:36:18 +0700

    nwe

--- testgit.c
+++ testgit.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 main(){
-	printf("Test");
-	printf("first");
+   float salary,a;
+   scanf("%f",&salary);
+   if (salary < 0){
+   	  printf("Error: Salary must be greater or equal to 0");
+   }
+   else{
+   	   if(salary<=300000){
+   	     	a = salary*0.05;
+   	   }
+       else{
+   		    a = 15000+((salary-300000)*0.1);
+   	   }
+   	   printf("%.2f",a);
+   }
 }
